684753939c2c1047e5189ad5c80b063793f7a675
nguyenkha1612 <19130093@st.hcmuaf.edu.vn>
2022-07-18 18:02:55 +0700

    Fix scss

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from "./web/components/pages/home/home.component";
+import {ProductListComponent} from "./web/components/pages/product-list/product-list.component";
+import {ProductDetailComponent} from "./web/components/pages/product-detail/product-detail.component";
+import {ContactComponent} from "./web/components/pages/contact/contact.component";
+import {AboutComponent} from "./web/components/pages/about/about.component";
+import {NotFoundComponent} from "./web/components/pages/not-found/not-found.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'product-list', component: ProductListComponent},
+    {path: 'product-detail/:id', component: ProductDetailComponent},
+    {path: 'contact', component: ContactComponent},
+    {path: 'about-us', component: AboutComponent},
+    {path: '**', component: NotFoundComponent},
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,33 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './web/home/home.component';
-import { ProductsComponent } from './web/products/products.component';
-import { ProductComponent } from './web/product/product.component';
-import { ContactComponent } from './web/contact/contact.component';
-import { AboutComponent } from './web/about/about.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './web/components/pages/home/home.component';
+import {ProductListComponent} from './web/components/pages/product-list/product-list.component';
+import {AboutComponent} from './web/components/pages/about/about.component';
+import {ContactComponent} from './web/components/pages/contact/contact.component';
+import {ProductDetailComponent} from './web/components/pages/product-detail/product-detail.component';
+import {HttpClientModule} from "@angular/common/http";
+import { NotFoundComponent } from './web/components/pages/not-found/not-found.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    ProductsComponent,
-    ProductComponent,
-    ContactComponent,
-    AboutComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        ProductListComponent,
+        AboutComponent,
+        ContactComponent,
+        ProductDetailComponent,
+        NotFoundComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
