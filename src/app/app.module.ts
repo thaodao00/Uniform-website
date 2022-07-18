27d2e235b19188c48147b470445e4121b5db0e51
Đào Thị Thu Thảo <91473426+thaodao00@users.noreply.github.com>
2022-07-18 16:01:28 +0700

    Create a project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './web/home/home.component';
+import { ProductsComponent } from './web/products/products.component';
+import { ProductComponent } from './web/product/product.component';
+import { ContactComponent } from './web/contact/contact.component';
+import { AboutComponent } from './web/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProductsComponent,
+    ProductComponent,
+    ContactComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
