8425760d49796e44d769f08fa227ecf6709dc7ba
Đào Thị Thu Thảo <91473426+thaodao00@users.noreply.github.com>
2022-07-19 10:39:15 +0700

    Product-list Screen
    Product-list screen development and api test run

--- src/app/web/components/pages/product-list/product-list.component.ts
+++ src/app/web/components/pages/product-list/product-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Product} from "../../../modules/product";
+import {OwlOptions} from "ngx-owl-carousel-o";
+import {ProductService} from "../../../services/product.service";
 
 @Component({
   selector: 'app-product-list',
@@ -6,10 +9,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-list.component.scss']
 })
 export class ProductListComponent implements OnInit {
+  products: Array<Product> = new Array<Product>();
 
-  constructor() { }
+  constructor(private productServices: ProductService) {
+
+  }
 
   ngOnInit(): void {
+    this.productServices.getList().subscribe(data=>{
+      this.products= data;
+      console.log(this.products)
+    })
+
   }
 
 }
